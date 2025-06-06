f07f21f176a43ad27f505dd64adf7fb5af4e687c
Pete Irvine <pete.irvine89@gmail.com>
2025-06-06 11:13:35 +0100

    📈 Add Vercel Analytics to App.js for traffic insights

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import SORSection from "./components/SORSection";
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 // 1) Fixed list of desired sections; “contractor work” stays last
 const desiredOrder = [
@@ -968,6 +969,7 @@ function App() {
             >
               Send Feedback
             </Button>
+            <Analytics />
           </Container>
         </>
       )}
